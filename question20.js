a4172a0262d1319262a3e8733b7633fc28f75c22
sohel <sohelabdul898@gmail.com>
2026-08-27 21:28:48 +0530

    add new file

---
+++ question20.js
@@ -0,0 +1,11 @@
+//  let i =1
+//  let n = 5
+//  while (i<=n) {
+//     console.log(i*i)
+//     i++
+//  }
+ 
+let n = 5
+for (let i = 1; i <=5; i++) {
+    console.log(i*i)
+}
---
+++ question21.js
@@ -0,0 +1,10 @@
+// let i = 1
+// let n = 10
+// while (i<=n) {
+//     console.log(i*i*i)
+//     i++
+// }
+let n =10
+for (let i = 1; i <=n; i++) {
+    console.log(i*i*i)
+}
