a42e5f0bb47b3d3431704b7496214cd7ddd26032
sohel <sohelabdul898@gmail.com>
2026-08-28 19:47:22 +0530

    add new file

---
+++ question22.js
@@ -0,0 +1,21 @@
+// let n = 6
+// let i =1
+// let sum =0
+// while (i<=n) {
+//     if (n%i==0) {
+//         sum+=i
+       
+//     }
+//     i++
+// }
+// console.log(sum)
+let sum = 0
+let n =6
+for (let i = 1; i <=n; i++) {
+    if (n%i==0) {
+        sum+=i
+        
+    }
+   
+}
+console.log(sum)
---
+++ question23.js
@@ -0,0 +1,15 @@
+// let n =24
+// let i =1
+// while(i<=n){
+//     if(n%i==0){
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+let n =24;
+for (let i = 1; i <=n; i++) {
+    if (n%i==0) {
+        console.log(i)
+    }
+}
