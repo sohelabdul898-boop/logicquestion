4408c3d0dd2be0a1356143235d587593485a876d
sohel <sohelabdul898@gmail.com>
2026-08-29 20:38:18 +0530

    add the new file

---
+++ question28.js
@@ -0,0 +1,16 @@
+// let a =10
+// let b =30
+// let n = a
+// while(n<=b){
+//     if(n%7==0){
+//          console.log(n) 
+//     }
+//     n= n+1
+    
+// }
+let a =10
+let b =30
+for(let n =a ; n<=b ; n=n+1){
+if(n%7==0)
+    console.log(n)
+}
