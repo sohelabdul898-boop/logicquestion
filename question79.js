fb83deba975bd492f55c215480f662d2852f4baf
sohel <sohelabdul898@gmail.com>
2026-09-25 20:28:42 +0530

    adding the new file

---
+++ question79.js
@@ -0,0 +1,12 @@
+// let n ="";
+// let row = 4;
+for(let i=0;i<=4;i++){
+    let arr= []
+    
+    for(let j =0;j<=4;j++){
+        //   n +="*";
+        arr.push("*")
+    }
+    console.log(arr.join(''))
+}
+
---
+++ question80.js
@@ -0,0 +1,7 @@
+for(let i =0;i<=5;i++){
+    let arr =[]
+    for(let j=0;j<-i;j++){
+        arr.push("*")
+    }
+    console.log(arr.join(''))
+}
